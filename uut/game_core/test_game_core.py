0a46cc2c7238d1ea67d3e2128fdfe961f720a3b2
Lnyan <lkwq007@gmail.com>
2017-07-10 15:59:48 +0800

    Update test_game_core

--- uut/game_core/test_game_core.py
+++ uut/game_core/test_game_core.py
@@ -14,6 +14,7 @@ from tkinter import *
 # using cocotb + tkinter + iverilog (modelsim can also work) to simluate game_core design
 width=50
 height=50
+
 def dut_init(dut):
 	dut.clk=0
 	dut.game_area=0
@@ -24,10 +25,10 @@ def dut_init(dut):
 	dut.reset=0
 	dut.cursor=0
 
-def key(event,dut,flag,block):
+def key(event,dut,block,flag):
 	print(event.char)
 	if event.char==27:
-		flag=False
+		flag.set(0)
 	elif event.char=='a':
 		dut.game_area=1-int(dut.game_area)
 	elif event.char=='e':
@@ -36,6 +37,10 @@ def key(event,dut,flag,block):
 		dut.retry=1-int(dut.retry)
 	elif event.char=='t':
 		dut.retract=1-int(dut.retract)
+	elif event.char=='j':
+		dut.left=1-int(dut.left)
+	elif event.char=='k':
+		dut.right=1-int(dut.right)
 	else:
 		block.set(0)
 
@@ -43,15 +48,16 @@ def motion(event,dut):
 	cursor=event.y//height*8+event.x//width
 	dut.cursor=cursor
 
-def lmouse(event,dut,block):
+def lmouse(event,dut,block,left):
 	cursor=event.y//height*8+event.x//width
 	dut.cursor=cursor
-	dut.left=1
+	#dut.left=1
+	left.set(1)
 	block.set(0)
 
 def draw_object(mask,canvas,color,size):
 	for i in range(64):
-		if mask[i]=='1':
+		if mask[63-i]=='1':
 			canvas.create_rectangle(i%8*width,i//8*height,i%8*width+size,i//8*height+size,fill=color)
 
 def gather_output(dut,canvas):
@@ -71,32 +77,63 @@ def gather_output(dut,canvas):
 
 @cocotb.test()
 def game_core_test(dut):
-	flag=True
 	# init sokoban gui
 	gui=Tk()
 	block=IntVar()
 	block.set(1)
+	flag=IntVar()
+	flag.set(1)
+	left=IntVar()
+	left.set(0)
 	canvas=Canvas(gui,width=400,height=400)
-	canvas.bind("<Key>",lambda event: key(event,dut,flag,block))
+	canvas.bind("<Key>",lambda event: key(event,dut,block,flag))
 	canvas.focus_set()
-	canvas.bind("<Button-1>",lambda event: lmouse(event,dut,block))
+	canvas.bind("<Button-1>",lambda event: lmouse(event,dut,block,left))
 	canvas.pack()
-	label=Label(gui,text="Reset: "+str(dut.reset)+"; Area: "+str(dut.game_area)+"; Retry: "+str(dut.retry)+"; Retract: "+str(dut.retract)+"; Stage: "+str(dut.stage)+"; Win:"+str(dut.win))
+	label=Label(gui,text="Reset: "+str(dut.reset)+"; Area: "+str(dut.game_area)+"; Retry: "+str(dut.retry)+"; Retract: "+str(dut.retract)+"; Stage: "+str(dut.stage)+"; Win:"+str(dut.win)+"; Left: "+str(dut.left)+"; Right: "+str(dut.right))
 	label.pack()
 	# init all input signal
 	dut_init(dut)
-	while flag:
-		dut.clk=0
-		yield Timer(100)
-		dut.clk=1
-		yield Timer(100)
-		gather_output(dut,canvas)
-		label.config(text="Reset: "+str(dut.reset)+"; Area: "+str(dut.game_area)+"; Retry: "+str(dut.retry)+"; Retract: "+str(dut.retract)+"; Stage: "+str(dut.stage)+"; Win:"+str(dut.win))
+	dut.clk=0
+	yield Timer(100)
+	dut.reset=1
+	dut.clk=1
+	yield Timer(100)
+	dut.clk=0
+	yield Timer(100)
+	dut.reset=0
+	dut.clk=1
+	yield Timer(100)
+	dut.clk=0
+	yield Timer(100)
+	while flag.get():
 		dut.left=0
+		if int(dut.controller.state)==2 or int(dut.controller.state)==3:
+			gui.wait_variable(left)
+			if left.get()==1:
+				dut.left=1
+			dut.clk=1
+			yield Timer(100)
+			dut.clk=0
+			yield Timer(100)
+		elif int(dut.controller.state)==4:
+			dut.clk=1
+			yield Timer(100)
+			dut.clk=0
+			yield Timer(100)
+			break
+		else:
+			gui.wait_variable(block)
+			dut.clk=1
+			yield Timer(100)
+			dut.clk=0
+			yield Timer(100)
+		dut._log.info("state: "+str(dut.controller.state))
 		dut._log.info("stage: "+str(dut.stage)+"win: "+str(dut.win))
-		gui.wait_variable(block)
-
+		label.config(text="Reset: "+str(dut.reset)+"; Area: "+str(dut.game_area)+"; Retry: "+str(dut.retry)+"; Retract: "+str(dut.retract)+"; Stage: "+str(dut.stage)+"; Win:"+str(dut.win))
+		gather_output(dut,canvas)
 		gui.update()
+		
 	x=input("quit?")
 	gui.quit()
 	yield Timer(200)
